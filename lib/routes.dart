7fa633bddd6ee440d87300b6c07ed52a8bae615f
eom <djaxorb3091@naver.com>
2024-12-12 11:33:54 +0900

    commit

---
+++ lib/routes.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'About.dart';
+import 'Home.dart';
+import 'def/Def.dart';
+
+//Java Map (HashMap Key, value)
+final Map<String, WidgetBuilder> routes = {
+  '/home': (context) => Home(),
+  '/about': (context) => About(),
+  '/def': (context) => Def(),
+};
